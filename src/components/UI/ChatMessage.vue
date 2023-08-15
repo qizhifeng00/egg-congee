d92b47a00f91474604f0b5707b5d20ec76a9469b
fengzhiqi <46121429+qizhifeng00@users.noreply.github.com>
2023-08-15 14:42:03 +0800

    第一次提交

---
+++ src/components/UI/ChatMessage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-chat-message :text="[text]" :sent="sent" />
+  1
+</template>
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    text: string;
+    sent?: boolean;
+  }>(),
+  {
+    text: '',
+    sent: false,
+  }
+);
+</script>
